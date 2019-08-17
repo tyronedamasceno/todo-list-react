2dfdc8163cc0974511d8d466228179d881429ddb
Tyrone Damasceno <tyronedamasceno@gmail.com>
2019-08-17 09:03:45 -0300

    Fix JS identation to 2 spaces

--- src/TodoItems.js
+++ src/TodoItems.js
@@ -2,68 +2,68 @@ import React, { Component } from "react";
 import FlipMove from "react-flip-move";
 
 class TodoItems extends Component {
-    constructor(props){
-        super(props);
-        
-        this.createTasks = this.createTasks.bind(this);
-    }
-    
-    createTasks(item) {
-        var status_mapping = {
-            0: 'Waiting',
-            1: 'In progress',
-            2: 'Done'
-        };
-        return <li key={item.id} className={`status-${item.status}`}>
-                    <div className="card-texts">
-                        <span className="status-name ">{status_mapping[item.status]}</span>
-                        <p className="task-name">{item.title}</p>
-                    </div>
-                    <div className="card-buttons">
-                        {(item.status === 0 && item.is_active) && (
-                            <button className="card-button"  onClick={() => this.update(item.id, {status: 1})}>
-                                Start
-                            </button>
-                        )}
-                        {(item.status === 1  && item.is_active) && (
-                            <button className="card-button" onClick={() => this.update(item.id, {status: 2})}>
-                                Finish
-                            </button>
-                        )}
-                        {(item.is_active) && (
-                            <button className="card-button" onClick={() => this.delete(item.id)}>
-                                Archive
-                            </button>
-                        )}
-                        {(!item.is_active) && (
-                            <button className="card-button" onClick={() => this.update(item.id, {is_active: true})}>
-                                Unarchive
-                            </button>
-                        )}
-                    </div>
-                </li>
-    }
+	constructor(props) {
+		super(props);
 
-    delete(key) {
-        this.props.delete(key);
-    }
-    
-    update(key, newStatus) {
-        this.props.update(key, newStatus);
-    }
+		this.createTasks = this.createTasks.bind(this);
+	}
 
-    render() {
-        var todoEntries = this.props.entries;
-        var listItems = todoEntries.map(this.createTasks);
+	createTasks(item) {
+		var status_mapping = {
+			0: 'Waiting',
+			1: 'In progress',
+			2: 'Done'
+		};
+		return <li key={item.id} className={`status-${item.status}`}>
+			<div className="card-texts">
+				<span className="status-name ">{status_mapping[item.status]}</span>
+				<p className="task-name">{item.title}</p>
+			</div>
+			<div className="card-buttons">
+				{(item.status === 0 && item.is_active) && (
+					<button className="card-button" onClick={() => this.update(item.id, { status: 1 })}>
+						Start
+                            </button>
+				)}
+				{(item.status === 1 && item.is_active) && (
+					<button className="card-button" onClick={() => this.update(item.id, { status: 2 })}>
+						Finish
+                            </button>
+				)}
+				{(item.is_active) && (
+					<button className="card-button" onClick={() => this.delete(item.id)}>
+						Archive
+                            </button>
+				)}
+				{(!item.is_active) && (
+					<button className="card-button" onClick={() => this.update(item.id, { is_active: true })}>
+						Unarchive
+                            </button>
+				)}
+			</div>
+		</li>
+	}
 
-        return (
-            <ul className= "theList">
-              <FlipMove duration={250} easing="ease-out">
-                {listItems}
-              </FlipMove>
-            </ul>
-        );
-    }
-} 
+	delete(key) {
+		this.props.delete(key);
+	}
+
+	update(key, newStatus) {
+		this.props.update(key, newStatus);
+	}
+
+	render() {
+		var todoEntries = this.props.entries;
+		var listItems = todoEntries.map(this.createTasks);
+
+		return (
+			<ul className="theList">
+				<FlipMove duration={250} easing="ease-out">
+					{listItems}
+				</FlipMove>
+			</ul>
+		);
+	}
+}
 
 export default TodoItems;
--- src/TodoList.js
+++ src/TodoList.js
@@ -3,137 +3,136 @@ import TodoItems from "./TodoItems";
 import "./TodoList.css";
 
 class TodoList extends Component {
-    static baseUrl = 'http://localhost:5000/api/v1';
-    constructor(props) {
-        super(props);
+	static baseUrl = 'http://localhost:5000/api/v1';
+	constructor(props) {
+		super(props);
 
-        this.state = {
-            items: []
-        };
+		this.state = {
+			items: []
+		};
 
-        this.getTasks = this.getTasks.bind(this);
-        this.addItem = this.addItem.bind(this);
-        this.deleteItem = this.deleteItem.bind(this);
-        this.updateTask = this.updateTask.bind(this);
-    }
+		this.getTasks = this.getTasks.bind(this);
+		this.addItem = this.addItem.bind(this);
+		this.deleteItem = this.deleteItem.bind(this);
+		this.updateTask = this.updateTask.bind(this);
+	}
 
-    componentDidMount() {
-        fetch(TodoList.baseUrl+'/tasks').then((response) => {
-          return response.json();  
-        }).then((responseData) => {
-            const sortedItems = responseData.data.sort((a, b) => a.status-b.status);
-            this.setState({ items: sortedItems});
-        }).catch(console.error);
-    }
+	componentDidMount() {
+		fetch(TodoList.baseUrl + '/tasks').then((response) => {
+			return response.json();
+		}).then((responseData) => {
+			const sortedItems = responseData.data.sort((a, b) => a.status - b.status);
+			this.setState({ items: sortedItems });
+		}).catch(console.error);
+	}
 
-    getTasks(endpoint) {
-        fetch(TodoList.baseUrl+'/tasks'+endpoint).then((response) => {
-            return response.json();
-        }).then((responseData) => {
-            const sortedItems = responseData.data.sort((a, b) => a.status-b.status);
-            this.setState({ items: sortedItems});
-        })
-    }
+	getTasks(endpoint) {
+		fetch(TodoList.baseUrl + '/tasks' + endpoint).then((response) => {
+			return response.json();
+		}).then((responseData) => {
+			const sortedItems = responseData.data.sort((a, b) => a.status - b.status);
+			this.setState({ items: sortedItems });
+		})
+	}
 
-    addItem(e) {
-        if (this._inputElement.value.trim() !== "") {
-            var newItem = {
-                title: this._inputElement.value
-            };
-            fetch(TodoList.baseUrl+'/tasks', {
-                headers: {'Content-Type': 'application/json'},
-                method: 'POST',
-                body: JSON.stringify(newItem)
-            }).then((response) => {
-                return response.json();
-            }).then((response) => {
-                newItem.id = response.data.id;
-                newItem.key = response.data.id;
-                newItem.status = response.data.status;
-                newItem.is_active = response.data.is_active;
-                
-                const items = this.state.items;
-                const newItems = items.concat(newItem);
-                const newSortedItems = newItems.sort((a, b) => a.status-b.status);
-                this.setState((prevState) => {
-                    return {
-                        items: newSortedItems
-                    };
-                });
-            });
-        }
+	addItem(e) {
+		if (this._inputElement.value.trim() !== "") {
+			var newItem = {
+				title: this._inputElement.value
+			};
+			fetch(TodoList.baseUrl + '/tasks', {
+				headers: { 'Content-Type': 'application/json' },
+				method: 'POST',
+				body: JSON.stringify(newItem)
+			}).then((response) => {
+				return response.json();
+			}).then((response) => {
+				newItem.id = response.data.id;
+				newItem.key = response.data.id;
+				newItem.status = response.data.status;
+				newItem.is_active = response.data.is_active;
 
-        this._inputElement.value = "";
+				const items = this.state.items;
+				const newItems = items.concat(newItem);
+				const newSortedItems = newItems.sort((a, b) => a.status - b.status);
+				this.setState((prevState) => {
+					return {
+						items: newSortedItems
+					};
+				});
+			});
+		}
 
-        e.preventDefault();
-    }
+		this._inputElement.value = "";
 
-    deleteItem(key) {
-        fetch(TodoList.baseUrl+'/task/'+key, {
-            method: 'DELETE'
-        }).then((response) =>{
-            var filteredItems = this.state.items.filter(function (item) {
-                return (item.id !== key);
-            });
-            this.setState({
-                items: filteredItems
-            });
-        });
-    }
+		e.preventDefault();
+	}
 
-    updateTask(key, params) {
-        fetch(TodoList.baseUrl+'/task/'+key, {
-            headers: {'Content-Type': 'application/json'},
-            method: 'PATCH',
-            body: JSON.stringify(params)
-        }).then((response) => {
-            return response.json();
-        }).then((res) => {
-            const response = res.updated_data;
-            const items = this.state.items;
-            items.map(item => { 
-                if (item.id === response.id) {
-                    item.status = response.status;
-                    item.is_active = response.is_active;
-                }
-                return item;
-            });
-            let sortedItems = items.sort((a, b) => a.status-b.status);
-            if (params.is_active) {
-                sortedItems = sortedItems.filter(item => item.id !== key);
-            }
-            this.setState({
-                items: sortedItems
-            });
-        });
-    }
+	deleteItem(key) {
+		fetch(TodoList.baseUrl + '/task/' + key, {
+			method: 'DELETE'
+		}).then((response) => {
+			var filteredItems = this.state.items.filter(function (item) {
+				return (item.id !== key);
+			});
+			this.setState({
+				items: filteredItems
+			});
+		});
+	}
 
-    render() {
-        return (
-            <div className="todoListMain">
-              <div className="header">
-                <input type="text" className="input" placeholder="Search..." />
-                <div className="header-buttons">
-                    <button onClick={() => this.getTasks('')}>Active</button>
-                    <button onClick={() => this.getTasks('/pending')}>Pending</button>
-                    <button onClick={() => this.getTasks('/done')}>Finished</button>
-                    <button onClick={() => this.getTasks('/archived')}>Archived</button>
-                </div>
-                <form onSubmit={this.addItem}>
-                  <input ref={(a) => this._inputElement = a}
-                         placeholder="enter task">
-                  </input>
-                  <button type="submit">add</button>
-                </form>
-              </div>
-              <TodoItems entries={this.state.items}
-                         delete={this.deleteItem}
-                         update={this.updateTask}
-                         />
-            </div>
-        )
-    }
+	updateTask(key, params) {
+		fetch(TodoList.baseUrl + '/task/' + key, {
+			headers: { 'Content-Type': 'application/json' },
+			method: 'PATCH',
+			body: JSON.stringify(params)
+		}).then((response) => {
+			return response.json();
+		}).then((res) => {
+			const response = res.updated_data;
+			const items = this.state.items;
+			items.map(item => {
+				if (item.id === response.id) {
+					item.status = response.status;
+					item.is_active = response.is_active;
+				}
+				return item;
+			});
+			let sortedItems = items.sort((a, b) => a.status - b.status);
+			if (params.is_active) {
+				sortedItems = sortedItems.filter(item => item.id !== key);
+			}
+			this.setState({
+				items: sortedItems
+			});
+		});
+	}
+
+	render() {
+		return (
+			<div className="todoListMain">
+				<div className="header">
+					<input type="text" className="input" placeholder="Search..." />
+					<div className="header-buttons">
+						<button onClick={() => this.getTasks('')}>Active</button>
+						<button onClick={() => this.getTasks('/pending')}>Pending</button>
+						<button onClick={() => this.getTasks('/done')}>Finished</button>
+						<button onClick={() => this.getTasks('/archived')}>Archived</button>
+					</div>
+					<form onSubmit={this.addItem}>
+						<input ref={(a) => this._inputElement = a}
+							placeholder="enter task">
+						</input>
+						<button type="submit">add</button>
+					</form>
+				</div>
+				<TodoItems entries={this.state.items}
+					delete={this.deleteItem}
+					update={this.updateTask}
+				/>
+			</div>
+		)
+	}
 }
 
 export default TodoList;
- 
